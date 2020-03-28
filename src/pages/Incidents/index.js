60469cb4d073d1672238b085b945da50317a7f38
Igor Souza <igorsouza.dev@gmail.com>
2020-03-28 10:38:35 -0300

    Adding pages

---
+++ src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Incidents() {
+  return (
+    <Container>
+      <Text>Incidents page</Text>
+    </Container>
+  );
+}
