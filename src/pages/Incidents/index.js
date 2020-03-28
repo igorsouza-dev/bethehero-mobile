5d1db0161d21be1f2f3341bc3008c6217ef01631
Igor Souza <igorsouza.dev@gmail.com>
2020-03-28 11:15:23 -0300

    Added Incidents page styles

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,12 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
+import { Image } from 'react-native';
+import logo from '../../assets/logo.png';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderText,
+  HeaderTextBold,
+  Title,
+  Description,
+  IncidentList,
+  Incident,
+  IncidentProperty,
+  IncidentValue,
+  DetailsButton,
+  DetailsButtonText,
+} from './styles';
 
 export default function Incidents() {
+  const navigation = useNavigation();
+  function navigateToDetail() {
+    navigation.navigate('Detail');
+  }
   return (
     <Container>
-      <Text>Incidents page</Text>
+      <Header>
+        <Image source={logo} />
+        <HeaderText>
+          Total de <HeaderTextBold>0 casos</HeaderTextBold>.
+        </HeaderText>
+      </Header>
+      <Title>Bem vindo!</Title>
+      <Description>Escolha um dos casos abaixo e salve o dia.</Description>
+      <IncidentList
+        data={[1, 2, 3]}
+        keyExtractor={(incident) => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={(item) => (
+          <Incident>
+            <IncidentProperty>ONG:</IncidentProperty>
+            <IncidentValue>APAD</IncidentValue>
+
+            <IncidentProperty>CASO:</IncidentProperty>
+            <IncidentValue>dfs fsd fsdf sd fsd</IncidentValue>
+
+            <IncidentProperty>VALOR:</IncidentProperty>
+            <IncidentValue>R$ 120,00</IncidentValue>
+
+            <DetailsButton onPress={() => navigateToDetail()}>
+              <DetailsButtonText>Detalhes</DetailsButtonText>
+              <Feather name="arrow-right" size={16} color="#e02041" />
+            </DetailsButton>
+          </Incident>
+        )}
+      />
     </Container>
   );
 }
